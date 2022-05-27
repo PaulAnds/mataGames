25baae0be39680bde7c4783fa23c2c4097c16016
unknown <mikes0174@hotmail.com>
2022-05-27 10:50:32 -0500

    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import { Platform } from 'react-native';
+
+import HomeScreen from './HomeScreen';
+import VideoGameScreen from './VideoGameScreen';
+import SettingsScreen from './SettingsScreen';
+
+const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
+
+const {createNavigator} = Platform.select({
+  ios:{createNavigator:Tab},
+  android:{createNavigator:Drawer},
+})
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <createNavigator.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+          if (route.name === 'Home') {
+            iconName = 'home-sharp';
+          } else if (route.name === 'Settings') {
+            iconName = 'list-sharp';
+          }else if (route.name === "Games"){
+            iconName = 'apps-sharp'; 
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >   
+        <createNavigator.Screen name= "Home" component={HomeScreen}/>
+        <createNavigator.Screen name= "Games" component={VideoGameScreen}/>
+        <createNavigator.Screen name= "Settings" component={SettingsScreen}/>
+      </createNavigator.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text,View,Image,StyleSheet } from "react-native";
+
+function HomeScreen(_props) {
+    return (
+        <View style={{flex:1,justifyContent: 'center',alignItems: 'center'}}>
+        <Text>Home!</Text>
+        </View>
+    );
+    
+}
+export default HomeScreen;
